3808795ba26fdd2433569091bbbe109920ed3599
antonol <anton.olson@hotmail.com>
2015-04-24 15:04:24 +0200

    Update ultra.c

--- ultra.c
+++ ultra.c
@@ -39,7 +39,7 @@ int getUltra() {
 }
 
 int getCM(){
- int val = 3;
+ int val = 5;
  int tmp=0;
  int sum=0;
  
@@ -47,7 +47,7 @@ int getCM(){
    tmp = getUltra();
    sum = sum+tmp;
    printf("%i\n", tmp);
-   delay(1000);
+   delay(100);
   }
   int res = sum/val;
   
