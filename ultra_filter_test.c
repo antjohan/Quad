709fdcd19659b92fa634a941305861facc6a4954
antonol <anton.olson@hotmail.com>
2015-04-27 08:36:26 +0200

    Create ultra_filter_test.c

---
+++ ultra_filter_test.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+ 
+// #define TRUE 1
+ 
+#define TRIG 4
+#define ECHO 5
+ 
+void ultraSetup() {
+        wiringPiSetup();
+        pinMode(TRIG, OUTPUT);
+        pinMode(ECHO, INPUT);
+ 
+        //TRIG pin must start LOW
+        digitalWrite(TRIG, LOW);
+        delay(30);
+}
+
+
+int getUltra() {
+        //Send trig pulse
+        digitalWrite(TRIG, HIGH);
+        delayMicroseconds(20);
+        digitalWrite(TRIG, LOW);
+ 
+        //Wait for echo start
+        while(digitalRead(ECHO) == LOW);
+ 
+        //Wait for echo end
+        long startTime = micros();
+        while(digitalRead(ECHO) == HIGH);
+        long travelTime = micros() - startTime;
+ 
+        //Get distance in cm
+        int distance = travelTime / 58;
+ 
+        return distance;
+}
+
+int getCM(){
+ int val = 5;
+ int tmp;
+ int sum;
+ 
+ 
+ tmp = 0;
+ sum = 0;
+  for(int i = 0; i<val; i++){
+ //  while(1){
+   tmp = getUltra();
+   sum = sum+tmp;
+   printf("%i\n", tmp);
+   delay(100);
+  }
+  int res_1 = sum/val;
+  
+  tmp = 0;
+    for(int i = 0; i<val; i++){
+ //  while(1){
+   tmp = getUltra();
+   sum = sum+tmp;
+   printf("%i\n", tmp);
+   delay(100);
+  }
+  int res_1 = sum/val;
+  
+  
+  
+  // Skriver till fil
+    FILE *fp;
+fp=fopen("data.txt","r+");
+//fprintf(fp, "%i", res, "\n");
+fclose(fp);
+  
+  
+  return res;
+  
+  
+}
