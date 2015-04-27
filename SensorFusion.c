2c92baefedc3b441f4f005f8d2da5cec7cda1f89
mrroack <oscar.wellenstam@telia.com>
2015-04-27 11:25:13 +0200

    lala

--- SensorFusion.c
+++ SensorFusion.c
@@ -1,10 +1,16 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+//#include <libconfig.h>
+
 //#include <unistd.h>
 
 #define MAX_BUF 256 //Amount of characters that can be sent through the fifos
  
+ //char * configfile= "./config.txt"
+ //config_t * config;
+
+
 int barometerfifofd;
 int ultrasonicfifofd;
 int magnetometerfifofd;
@@ -18,7 +24,17 @@ double coordinate[3]; //lat, long, quality from gps
 double speed; //info from gps
 
 
-int main(){}
+
+int main(){
+	/*
+	config_init(config)
+	config_read_file(config,configfile);
+	double test;
+	config_lookup_float(config,"sensors.magnetometer.sampling",test);
+	printf("%f", test);
+	*/
+
+}
 void init(){ 
 	//name of all fifos, will need to be matching in respective sensor programs
 	char* barometerfifo = "./tmp/barometerfifo";
