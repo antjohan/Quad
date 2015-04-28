61edae98198f6fe276679b98f11bbc67f03e9c43
mrroack <oscar.wellenstam@telia.com>
2015-04-28 10:45:15 +0200

    1

--- BMP180.c
+++ BMP180.c
@@ -17,7 +17,7 @@ void Initialize(){
   char* barometerfifo = "/tmp/barometerfifo";
   //delete in case it already exists
   unlink(barometerfifo);
-  delay (300)
+  delay (300);
   int a = mkfifo(barometerfifo,0666);
   if (a==-1){
     printf("mkfifoerror: %s\n",strerror(errno));
