a9d7f9c1d807d2480ec72b401fc2f0bfac081acc
mrroack <oscar.wellenstam@telia.com>
2015-04-28 11:37:11 +0200

    1

--- ultra.c
+++ ultra.c
@@ -89,76 +89,70 @@ long getUltra() {
 // I funktionen/filtret kan man välja antal mätningar(val) 
 
 long getCM(){
- long val = 10;
- long sum;
+   long val = 10;
+   long sum;
 
- 
- // Första mätningen, val=antal mätvärden,
- // värden större än 400cm förkastas
- long m1[val];
- long tmp;
- long medel1 = 0;
- long length1 = 0;
- 
-  for(int i = 0; i<val; i++){
-   tmp = getUltra();         // Gör en mätning
-   if(tmp < 400 && tmp > 0){            // Kollar om värdet är rimligt (kortare än 400cm)
-    m1[length1] = tmp;       // Sparar undan värdet
-    length1 = length1 + 1;   // Ökar indexeringen
-    medel1 = tmp + medel1;   // Summerar för att kunna göra medelvärde
-    }
-    if (length1 == 0){
-     printf("Avståndet är längre än 4m\n");
-     return ((long)-1);
-    }
-   delay(50);               // Fördröjning för att det ska fungera, tror 30 är lagom
-  }
-  
-  // Mätningarnas medelvärde 
-  long medel = medel1  / length1;
 
- // Mätvärdenas individuella differens från medelvärdet (diff)
- // och medelvärde av avvikelssen (diff_medel)
-
- long diff[length1];    // Vektor att fylla med individuella skillnader
- long diff_medel = 0;
-
- for(int i = 0; i<length1; i++){
- diff[i] = abs(medel1 - m1[i]);        // Differens i längd
- diff_medel = diff_medel + diff[i];  // Summerar alla differenser för att kunna ta medelvärde
- }
- diff_medel = diff_medel/length1;      // Tar medevärdet
-
-// Filtrerar bort de mätningar som är vars avvikelse är 
-// längre bort än medelavvikelsen + 5 cm
- long count = 0;                      // Räknare för indexering då vektorn blir kortare
- long res[val];
- for(int i = 0; i<length1; i++){
-  if(diff[i] < diff_medel + 5 ){     // Krav för att bli godkänd
-   res[count] = m1[i];                // Skrivs in i vektorn
-   count = count + 1;                // Indexering ökar
+   // Första mätningen, val=antal mätvärden,
+   // värden större än 400cm förkastas
+   long m1[val];
+   long tmp;
+   long medel1 = 0;
+   long length1 = 0;
+   for(int i = 0; i<val; i++){
+      tmp = getUltra();         // Gör en mätning
+      if(tmp < 400 && tmp > 0){            // Kollar om värdet är rimligt (kortare än 400cm)
+         m1[length1] = tmp;       // Sparar undan värdet
+         length1 = length1 + 1;   // Ökar indexeringen
+         medel1 = tmp + medel1;   // Summerar för att kunna göra medelvärde
+      }
+      if (length1 == 0){
+      printf("Avståndet är längre än 4m\n");
+      return ((long)-1);
+      }
+      delay(50);               // Fördröjning för att det ska fungera, tror 30 är lagom
    }
- }
- 
- 
- // De mätvärden som är godkända summeras och medelvärdes tas
- long ans = 0;                       
- for(int i = 0; i<count; i++){ 
-  ans = ans + res[i];             
- }
- ans = ans/count;
- 
- 
-/*  // Skriver till fil
-    FILE *fp;
-fp=fopen("data.txt","r+");
-//fprintf(fp, "%ld\n", ans);
-fclose(fp);
-*/
+   // Mätningarnas medelvärde 
+   long medel = medel1  / length1;
+   // Mätvärdenas individuella differens från medelvärdet (diff)
+   // och medelvärde av avvikelssen (diff_medel)
+   long diff[length1];    // Vektor att fylla med individuella skillnader
+   long diff_medel = 0;
+   for(int i = 0; i<length1; i++){
+      diff[i] = abs(medel1 - m1[i]);        // Differens i längd
+      diff_medel = diff_medel + diff[i];  // Summerar alla differenser för att kunna ta medelvärde
+   }
+   diff_medel = diff_medel/length1;      // Tar medevärdet
 
-printf("%ld\n", ans);
- }
-  return ((long)ans);
+   // Filtrerar bort de mätningar som är vars avvikelse är 
+   // längre bort än medelavvikelsen + 5 cm
+   long count = 0;                      // Räknare för indexering då vektorn blir kortare
+   long res[val];
+   for(int i = 0; i<length1; i++){
+      if(diff[i] < diff_medel + 5 ){     // Krav för att bli godkänd
+         res[count] = m1[i];                // Skrivs in i vektorn
+         count = count + 1;                // Indexering ökar
+      }
+   }
+
+
+   // De mätvärden som är godkända summeras och medelvärdes tas
+   long ans = 0;                       
+   for(int i = 0; i<count; i++){ 
+      ans = ans + res[i];             
+   }
+   ans = ans/count;
+
+
+   /*  // Skriver till fil
+   FILE *fp;
+   fp=fopen("data.txt","r+");
+   //fprintf(fp, "%ld\n", ans);
+   fclose(fp);
+   */
+
+   printf("%ld\n", ans);
+   return ((long)ans);
 }
 
 void getCMloop(){
