01f52b81551b20da56a089a053c5c6a1a528514f
mrroack <oscar.wellenstam@telia.com>
2015-04-30 09:50:40 +0200

    1

--- BMP180.c
+++ BMP180.c
@@ -64,7 +64,7 @@ void setCurrentHeight(float height){ //readjust the barometer's height(relative 
 
  void sample(){
   float RelativeAltitude;
-  char WriteBuf[56];
+  char WriteBuf[MAX_BUF];
   while(sampling==1){
     float AltitudeSum=0;
     for (int i=0;i<4;++i){
--- BMP180.h
+++ BMP180.h
@@ -33,6 +33,7 @@
  #define BMP180_Mode_Standard      1
  #define BMP180_Mode_HighResolution    2
  #define BMP180_Mode_UltraHighResolution 3
+ #define MAX_BUF 56
 
 long InitialPressurePa=101300;
 int from_baro_fd;
