af6f231e013db7bb20874e0e24a21a377006898d
mrroack <oscar.wellenstam@telia.com>
2015-05-04 10:13:14 +0200

    1

--- SensorFusion.c
+++ SensorFusion.c
@@ -140,7 +140,7 @@ void sfinit(){
  	*/
  	printf("to_fifos=connected!\nFIFOS CONNECTED SUCCESSFULLY\n");
 //initialize flight log
-	fusionlogfd=open(fusionlog, O_WRONLY);
+	fusionlogfd=open(to_log_file, O_WRONLY);
 	t1=clock();
 }
 
