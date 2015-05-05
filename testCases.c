e216b33aee3fa3292aad79e79301208c63123dc3
tyggisn <tyggisn@gmail.com>
2015-05-06 01:26:45 +0200

    Update testCases.c

--- testCases.c
+++ testCases.c
@@ -32,7 +32,7 @@ float PIDcal(float diff) {
 	float dt = 0.01; //100ms loop time 
 	float MAX = 5;  //for Current Saturation 
 	float MIN = -5; //hoverconst-..
-	float Kp = 15;
+	float Kp = 12;
 	float Kd = 0.2;
 	float Ki = 0.2;
 	float error = diff; 
