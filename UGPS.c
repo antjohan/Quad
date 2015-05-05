a4ad485fe279b5598324be936440b82de3ceace7
mrroack <oscar.wellenstam@telia.com>
2015-05-05 13:12:52 +0200

    1

--- UGPS.c
+++ UGPS.c
@@ -1,5 +1,8 @@
  #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
  #include <wiringPiI2C.h>
  #include <wiringPi.h>
  #include <inttypes.h>
@@ -11,10 +14,84 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#include "rtklib.h"
+
+char * address= "GPSsocket";
 
 
+int main()
+{
+    char c;
+    FILE *fp;
+    int fromlen;
+    register int i, s, ns, len;
+    struct sockaddr_un saun, fsaun;
 
-int main(){
+    /*
+     * Get a socket to work with.  This socket will
+     * be in the UNIX domain, and will be a
+     * stream socket.
+     */
+    if ((s = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
+        perror("server: socket");
+        exit(1);
+    }
 
+    /*
+     * Create the address we will be binding to.
+     */
+    saun.sun_family = AF_UNIX;
+    strcpy(saun.sun_path, ADDRESS);
+
+    /*
+     * Try to bind the address to the socket.  We
+     * unlink the name first so that the bind won't
+     * fail.
+     *
+     * The third argument indicates the "length" of
+     * the structure, not just the length of the
+     * socket name.
+     */
+    unlink(ADDRESS);
+    len = sizeof(saun.sun_family) + strlen(saun.sun_path);
+
+    if (bind(s, &saun, len) < 0) {
+        perror("server: bind");
+        exit(1);
+    }
+
+    /*
+     * Listen on the socket.
+     */
+    if (listen(s, 5) < 0) {
+        perror("server: listen");
+        exit(1);
+    }
+
+    /*
+     * Accept connections.  When we accept one, ns
+     * will be connected to the client.  fsaun will
+     * contain the address of the client.
+     */
+    if ((ns = accept(s, &fsaun, &fromlen)) < 0) {
+        perror("server: accept");
+        exit(1);
+    }
+
+    /*
+     * We'll use stdio for reading the socket.
+     */
+    fp = fdopen(ns, "r");
+
+    /*
+     * Then we read some strings from the client and
+     * print them out.
+     */
+	while(1){}
+        while ((c = fgetc(fp)) != EOF) {
+            putchar(c);
+
+            if (c == '\n')
+                break;
+        }
+    }
 }
