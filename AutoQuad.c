467166948de1dd485a78b49bf4b1fa7934b67840
mrroack <oscar.wellenstam@telia.com>
2015-05-05 17:14:27 +0200

    1

--- AutoQuad.c
+++ AutoQuad.c
@@ -198,7 +198,7 @@ int main(){
 					} else if (recdataprompt==7){
 						while(1){
 							printf("H: %lf\n",getHeight());
-							delay(50);
+							delay(200);
 						}
 					}else if (recdataprompt==8){
 						recdataprompt=-1;
