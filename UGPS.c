aaafdac5be1f4d712aee702e9470df8b87887263
mrroack <oscar.wellenstam@telia.com>
2015-05-05 10:53:27 +0200

    1

---
+++ UGPS.c
@@ -0,0 +1,22 @@
+ #include <stdio.h>
+#include <stdlib.h>
+ #include <wiringPiI2C.h>
+ #include <wiringPi.h>
+ #include <inttypes.h>
+ #include <stdbool.h>
+ #include <math.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <rtklib.h>
+
+
+Ublox GPS;
+
+int main(){
+	GPS.testConnection();
+
+}
