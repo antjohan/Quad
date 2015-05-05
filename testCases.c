2b2b362f6e7ee8005649d4e4c8e568e9f5cedfc6
tyggisn <tyggisn@gmail.com>
2015-05-05 08:40:21 +0200

    Update testCases.c

--- testCases.c
+++ testCases.c
@@ -85,8 +85,8 @@ int testHoverToStep(void){
   printf("servo satt\n");
 //  ultraSetup();//Hårdkod, ska bort senare
   for (int i = 0; i<100;i++){
-    long currentHeight = getHeight(); //Use the ultra sensor to get height
-    printf("#Iteration = %i, Höjd = %ld\n", i, currentHeight);
+    double currentHeight = getHeight(); //Use the ultra sensor to get height
+    printf("#Iteration = %i, Höjd = %d\n", i, currentHeight);
     delay(40);
   }
   printf("Klar med test, setter hover\n");
