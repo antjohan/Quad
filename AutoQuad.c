019ca517d33040e13de8fdd2360a010c04a83f83
antjohan <ajohansson.93@gmail.com>
2015-05-07 11:30:05 +0200

    1

--- AutoQuad.c
+++ AutoQuad.c
@@ -7,6 +7,7 @@
 //#include "BMP180.c"
 //#include "ultra.c"
 #include "SensorFusion.c"
+#include <libconfig.h>
 
 /*
 Pinout FlightController -> Servo_Num -> Raspberry Pi
@@ -30,10 +31,10 @@ void Set_Servo(int num, int pos){
 		snprintf(cmd, sizeof cmd, "%s %i%s%i%s %s", echo, num, equal,pos, procent, destination);
 		system(cmd);
 		//printf("%s\n",cmd);
-	}else{
-		printf("Invalid!");
+		}else{
+			printf("Invalid!");
+		}
 	}
-}
 
 #include "testCases.c" //Test!
 
@@ -79,7 +80,8 @@ int main(){
 		printf("[2]	Disarm FlightController\n");
 		printf("[3]	Set Servo\n");
 		printf("[4]	Test prog\n");
-		printf("[5]	Quit\n");
+		printf("[5]	Print config\n");
+		printf("[6]	Quit\n");
 		printf("---------------------------------\n");
 
 		int val;
@@ -90,41 +92,41 @@ int main(){
 			printf("Done!\n");
 
 
-		}else if( val == 2){
-			printf("Disarming...\n");
-			Disarm_FlightController();
-			printf("Done!\n");
+			}else if( val == 2){
+				printf("Disarming...\n");
+				Disarm_FlightController();
+				printf("Done!\n");
 
-		}else if(val == 3){
-			int servo;
-			int speed;
+				}else if(val == 3){
+					int servo;
+					int speed;
 
-			printf("[1] Set Aileron\n");
-			printf("[2] Set Elevation\n");
-			printf("[3] Set Throttle\n");
-			printf("[4] Set Ruder\n");
-			scanf("%d",&servo);
+					printf("[1] Set Aileron\n");
+					printf("[2] Set Elevation\n");
+					printf("[3] Set Throttle\n");
+					printf("[4] Set Ruder\n");
+					scanf("%d",&servo);
 
-			printf("Set Speed (0-100)\n");
-			scanf("%d", &speed);
+					printf("Set Speed (0-100)\n");
+					scanf("%d", &speed);
 
-			Set_Servo(servo, speed);
+					Set_Servo(servo, speed);
 
-		}
-		else if(val == 4){
-			int test;
-			printf("---------------------------------\n");
-			printf("[1]	ping...\n");
-			printf("[2]	recieve data\n");
-			printf("[3]	Testa hoverToStep\n");
-			printf("[4]	Testa ett varv rotation\n");
-			printf("[5]	Calibrate magnetometer\n");
-			printf("[6]	Test PID regulator\n");
-			printf("[7]	Set hover\n");
-			printf("[8]	PIDtest, 0.3m\n");
-			printf("[9]	Tillbaks\n");
-			printf("---------------------------------\n");
-			scanf("%d",&test);
+				}
+				else if(val == 4){
+					int test;
+					printf("---------------------------------\n");
+					printf("[1]	ping...\n");
+					printf("[2]	recieve data\n");
+					printf("[3]	Testa hoverToStep\n");
+					printf("[4]	Testa ett varv rotation\n");
+					printf("[5]	Calibrate magnetometer\n");
+					printf("[6]	Test PID regulator\n");
+					printf("[7]	Set hover\n");
+					printf("[8]	PIDtest, 0.3m\n");
+					printf("[9]	Tillbaks\n");
+					printf("---------------------------------\n");
+					scanf("%d",&test);
 			//barometertest
 			if (test == 1){
 				int pingprompt=0;
@@ -141,22 +143,22 @@ int main(){
 					scanf("%d",&pingprompt);
 					if(pingprompt==1){
 						commandSensor("baro\0", "ping\0");
-					} else if (pingprompt==2){
-						commandSensor("ultra\0", "ping\0");
-					} else if (pingprompt==3){
-						commandSensor("mag\0", "ping\0");
-					} else if (pingprompt==4){
-						commandSensor("gps\0", "ping\0");
-					} else if (pingprompt==5){
-						commandSensor("baro\0", "ping\0");
-						commandSensor("ultra\0", "ping\0");
-						commandSensor("mag\0", "ping\0");
-						commandSensor("gps\0", "ping\0");
-					} else if (pingprompt==6){
-						pingprompt=-1;
-					}
-				}
-			}
+						} else if (pingprompt==2){
+							commandSensor("ultra\0", "ping\0");
+							} else if (pingprompt==3){
+								commandSensor("mag\0", "ping\0");
+								} else if (pingprompt==4){
+									commandSensor("gps\0", "ping\0");
+									} else if (pingprompt==5){
+										commandSensor("baro\0", "ping\0");
+										commandSensor("ultra\0", "ping\0");
+										commandSensor("mag\0", "ping\0");
+										commandSensor("gps\0", "ping\0");
+										} else if (pingprompt==6){
+											pingprompt=-1;
+										}
+									}
+								}
 			//Ultra sensor
 			else if(test == 2){
 				int recdataprompt=0;
@@ -175,6 +177,7 @@ int main(){
 					scanf("%d",&recdataprompt);
 					if(recdataprompt==1){
 						printf("Barometer height(m): %lf\n", getBHeight());
+<<<<<<< Updated upstream
 					} else if (recdataprompt==2){
 						printf("Ultrasonic height(cm): %lf\n", getUHeight());
 					} else if (recdataprompt==3){
@@ -208,28 +211,67 @@ int main(){
 					}
 				}
 			}
+=======
+						} else if (recdataprompt==2){
+							printf("Ultrasonic height(cm): %lf\n", getUHeight());
+							} else if (recdataprompt==3){
+								printf("Magnetometer heading(deg): %lf\n", getHeading());
+								} else if (recdataprompt==4){
+									printf("GPS coordinates(lat/long/quality): %lf\n", getBHeight());
+									} else if (recdataprompt==5){
+										printf("Barometer height(m): %lf\n", getBHeight());
+										printf("Ultrasonic height(cm): %lf\n", getUHeight());
+										printf("Magnetometer heading(deg): %lf\n", getHeading());
+										printf("GPS coordinates(lat/long/quality): %lf\n", getBHeight());
+										} else if (recdataprompt==6){
+											while(1){
+												double uh=getUHeight();
+												double bh=getBHeight();
+												double h=getHeight(uh,bh);
+												double hd=getHeading();
+
+												updateLog(bh, uh, h, hd);
+
+												printf("Barometer: %lf   Ultrasonic: %lf  Height: %lf  Magnetometer: %lf\n",bh,uh,h, hd);
+												delay(50);
+											}
+											} else if (recdataprompt==7){
+												while(1){
+													printf("H: %lf\n",getHeight(getUHeight(),getBHeight()));
+													delay(200);
+												}
+												}else if (recdataprompt==8){
+													recdataprompt=-1;
+												}
+											}
+										}
+>>>>>>> Stashed changes
 			//Test hoverToStep
 			else if(test == 3){
 				testHoverToStep();
-			}else if(test == 4){
-				testRotation();
-			}
-			else if(test == 5){
-				commandSensor("mag", "calibrate");
-			}else if(test == 6){
-				PIDcal(1.2);	
-				printf("Test klart");
-			}else if(test == 7){
-				setHover();
-			}else if(test == 8){
-				pidInsideTest(0.3);
-			}else{
-				printf("Invalid\n");				
-			}
-			
+				}else if(test == 4){
+					testRotation();
+				}
+				else if(test == 5){
+					commandSensor("mag", "calibrate");
+					}else if(test == 6){
+						PIDcal(1.2);	
+						printf("Test klart");
+						}else if(test == 7){
+							setHover();
+							}else if(test == 8){
+								pidInsideTest(0.3);
+								}else{
+									printf("Invalid\n");				
+								}
+
+
 		}else if (val == 5){
+			int rate = Quad.Sensors.RefresRate;
+			print("%d", rate);
+		}else if (val == 6){
 			break;
-		}else if(val==6){//recieve barometer data
+		//}else if(val==6){//recieve barometer data
 			//while(1){
 				//printf("Barometer: %lf   Ultrasonic: %lf\n",getBHeight(),getUHeight());
 				//delay(100);
