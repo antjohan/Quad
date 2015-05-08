4edcfee3561422b593d751fd52bb712ff7829a84
mrroack <oscar.wellenstam@telia.com>
2015-05-08 12:00:06 +0200

    kj

--- UGPS.c
+++ UGPS.c
@@ -20,7 +20,7 @@
 int from_gps_fd;
 
 int main(){
-    pipe1();
+    socketxy();
 }
 int pipe1(){
        char* from_gps_fifo = "/home/pi/tmp/from_gps_fifo";
@@ -133,4 +133,49 @@ int socketxx()
         	}
     	}
     }
+}
+#include <netinet/in.h>
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int socketxy()
+{
+    int argc=23;
+    char *argv[]=23;
+     int sockfd, newsockfd, portno, clilen;
+     char buffer[256];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     printf("got this far!\n");
+     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+     if (newsockfd < 0) 
+          error("ERROR on accept");
+     bzero(buffer,256);
+     n = read(newsockfd,buffer,255);
+     if (n < 0) error("ERROR reading from socket");
+     printf("Here is the message: %s\n",buffer);
+     n = write(newsockfd,"I got your message",18);
+     if (n < 0) error("ERROR writing to socket");
+     return 0; 
 }
