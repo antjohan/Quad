a8700b3496ba14a0b44e23cce5062db50340c07f
mrroack <oscar.wellenstam@telia.com>
2015-05-08 12:02:56 +0200

    1

--- UGPS.c
+++ UGPS.c
@@ -144,21 +144,19 @@ void error(char *msg)
 
 int socketxy()
 {
-    int argc=23;
-    char *argv[]=23;
-     int sockfd, newsockfd, portno, clilen;
+    int portno=23;
+     int sockfd, newsockfd, clilen;
      char buffer[256];
      struct sockaddr_in serv_addr, cli_addr;
      int n;
-     if (argc < 2) {
+     /*if (argc < 2) {
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
-     }
+     }*/
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
      if (sockfd < 0) 
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
