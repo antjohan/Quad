4e86763cfdc96c6e6fffc9267b7093da7b8f3514
mrroack <oscar.wellenstam@telia.com>
2015-05-12 08:37:53 +0200

    1

--- AutoQuad.c
+++ AutoQuad.c
@@ -209,7 +209,7 @@ int main(){
                         printf("GPS coordinates(lat/long/quality): %lf\n", getBHeight());
                         
                     } else if (recdataprompt==6){
-                        while (1){
+                        while (kbhit()){
 
                             double uh=getUHeight();
                             double bh=getBHeight();
