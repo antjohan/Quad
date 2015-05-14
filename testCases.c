8a2d5d5c7ed7d2d1da86cc4edca73a1b1c724aab
mrroack <oscar.wellenstam@telia.com>
2015-05-14 17:12:34 +0200

    k

--- testCases.c
+++ testCases.c
@@ -133,7 +133,6 @@ Setup: 	Doubled inputs from RC reciever to the MUX on all ports except from yaw
 */
 
 void testRotation(){
-
   int i=0;
   while (i<50){
     printf("M:%lf\n",getHeading());
@@ -142,34 +141,34 @@ void testRotation(){
   }
 
 //get_time();
-//  char fname[50];
-//  char file_cmd[128];
-//  char dir[128];
-//  sprintf(fname, "%s speed=%d.txt", str_time, YawSpeed);
-//  sprintf(dir, "/home/pi/logs/Rotation/\"%s\"", fname);
+  char fname[50];
+  char file_cmd[128];
+  char dir[128];
+  sprintf(fname, "%s speed=%d.txt", str_time, YawSpeed);
+  sprintf(dir, "/home/pi/logs/Rotation/\"%s\"", fname);
 
-//  FILE *fp;
-//  fp=fopen(dir,"w");
+  FILE *fp;
+  fp=fopen(dir,"w");
  
   printf("Startar yaw rotations test, setter hover\n");
- 
-  //setHover();
-  //sleep(4);
+  
+  setHover();
+  sleep(4);
   
   printf("Initierar clockwise rotation\n"); 
   
-  //double start_time = millis();
-  //Set_Servo(4,50+YawSpeed);
+  double start_time = millis();
+  Set_Servo(4,50+YawSpeed);
 
   
   for(int i = 0; i < 80; i++){
-        double head=getHeading();
-// 	fprintf(fp, "Time = %lf, Grader = %lf\n", millis()-start_time, head);
-  	//printf("Time = %lf, Grader = %lf\n", millis()-start_time, head);
+    double head=getHeading();
+  	fprintf(fp, "Time = %lf, Grader = %lf\n", millis()-start_time, head);
+  	printf("Time = %lf, Grader = %lf\n", millis()-start_time, head);
   	printf("%lf\n", head);
   	delay(50);
   }
-/*
+
   fprintf(fp, "Klar med en rotation, börjar nästa\n");
   setHover();
   printf("Klar med clockwise, setter hover och mäter bromsning\n");
@@ -199,7 +198,7 @@ void testRotation(){
 	fprintf(fp, "Time = %lf, Grader = %lf\n", millis()-start_time, head);
 	printf("Time = %lf, Grader = %lf\n", millis()-start_time, head);
   	delay(50);
-  }*/
+  }
   //fclose(fp);
   //setHover();
   
